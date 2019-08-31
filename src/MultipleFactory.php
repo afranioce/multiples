a1b86d5d1ca6e2ab68139a38ee6abe16182bce4e
Afranio Martins <afraniomartins@konstroi.com>
2019-08-31 12:31:14 -0300

    feat: adiciona checagem de tipagem do php

--- src/MultipleFactory.php
+++ src/MultipleFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 class MultipleFactory
@@ -21,6 +23,6 @@ class MultipleFactory
                 return $multiple->getReplacer();
             }
         }
-        return $number;
+        return (string)$number;
     }
 }
--- src/Multiples/Five.php
+++ src/Multiples/Five.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Multiples;
 
 use App\Multiples\MultipleInterface;
